77c8e483c06606e5ef680a2dfc10194f62c222c1
NathalieLima <nathalielima3@gmail.com>
2023-11-30 15:28:35 -0300

    Adicionando classes ListaUsuarios e ListaSessoes

---
+++ src/classes/ListaUsuarios.java
@@ -0,0 +1,16 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class ListaUsuarios {
+    private ArrayList<String> lista_usuarios;
+
+    public void criarUsuario(Usuario usuario) {
+
+    }
+
+    public void removerUsuario(Usuario usuario) {
+
+    }
+
+}
