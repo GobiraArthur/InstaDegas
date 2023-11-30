e655c624591a61d28be1aa05e2d72b0a64cb356e
NathalieLima <nathalielima3@gmail.com>
2023-11-30 15:22:21 -0300

    Commit inicial

---
+++ src/classes/Sessao.java
@@ -0,0 +1,37 @@
+package classes;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Sessao {
+	private LocalDateTime data_hora_inicio;
+	private LocalDateTime data_hora_fim;
+	
+	public static void logar() {
+		
+	}
+	
+	public void deslogar() {
+			
+	}
+	
+	public void solicitaAutenticacao() {
+		
+	}
+	
+	public void criarAmizade() {
+		
+	}
+	
+	public void desfazerAmizade() {
+		
+	}
+	
+	public void criarPostagem() {
+		
+	}
+	
+	public static void listarPostagens() {
+		
+	}
+}
